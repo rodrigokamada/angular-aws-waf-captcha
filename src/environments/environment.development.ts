6595bccb424e39fb683ad701179e9d4b73949806
Rodrigo Kamada <rodrigo@kamada.com.br>
2026-01-09 19:22:51 -0300

    Adding the JavaScript URL.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+  aws: {
+    waf: {
+      url: 'https://a162a8302aaa.edge.captcha-sdk.awswaf.com/a162a8302aaa/jsapi.js',
+    },
+  },
+};
