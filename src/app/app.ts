74e3d3315a8eac2719bd9a163186d8a0f80f5fea
Rodrigo Kamada <rodrigo@kamada.com.br>
2026-01-10 09:55:49 -0300

    Adding the captcha implementation.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,62 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, ViewChild, model } from '@angular/core';
+
+import { environment } from '../environments/environment';
+
+declare global {
+  interface Window {
+    AwsWafCaptcha: any;
+  }
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('angular-aws-waf-captcha');
+export class App implements AfterViewInit {
+
+  @ViewChild('captcha') captcha!: ElementRef;
+  messageSuccess = model<string>('');
+  messageError = model<string>('');
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    this.loadScript();
+  }
+
+  private loadScript(): void {
+    if (document.getElementById('awsWafScript')) {
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.id = 'awsWafScript';
+    script.async = false;
+    script.defer = true;
+    script.src = environment.aws.waf.url;
+    script.onload = () => {
+      this.renderCaptcha();
+    };
+    document.head.appendChild(script);
+  }
+
+  private renderCaptcha(): void {
+    if (window.AwsWafCaptcha) {
+      window.AwsWafCaptcha.renderCaptcha(this.captcha.nativeElement, {
+        onSuccess: (token: string) => {
+          this.messageSuccess.set(`Captcha successfully validated! Token: ${token}`);
+          this.messageError.set('');
+        },
+        onError: (error: any) => {
+          this.messageError.set(`Error validating captcha: ${error}`);
+          this.messageSuccess.set('');
+        },
+      });
+    }
+  }
+
 }
